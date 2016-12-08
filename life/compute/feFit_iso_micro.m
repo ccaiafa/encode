ed368297d060fdf67b9e52d76d71c040093fda49
Cesar Caiafa <ccaiafa@gmail.com>
2016-12-08 13:33:45 -0500

    DEB: feFit_iso_micro funcion fixed

--- life/compute/feFit_iso_micro.m
+++ life/compute/feFit_iso_micro.m
@@ -9,31 +9,31 @@ function [fit] = feFit_iso_micro(M, w, bval, bvecs, S0, dSig)
 % Compute signal contributed by fascicles (with mean)
 %Sf = M_times_w_with_mean(M,w);
 nTheta = size(M.Dict,1);
-Nvoxels = size(M.Phi,2);
+nVoxels = size(M.Phi,2);
 Sf = M_times_w(M.Phi.subs(:,1),M.Phi.subs(:,2),M.Phi.subs(:,3),M.Phi.vals,M.Dict,w,nTheta,nVoxels);
 
-Sf = reshape(Sf,[nTheta,Nvoxels]);
+Sf = reshape(Sf,[nTheta,nVoxels]);
 
 % Extract unique bval
 bval = bval(1);
 
 % Compute sum of weights per voxel
 nFibers = size(M.Phi,3);
-A = sparse(M.Phi.subs(:,2),M.Phi.subs(:,3),ones(nnz(M.Phi),1),Nvoxels,nFibers);
+A = sparse(M.Phi.subs(:,2),M.Phi.subs(:,3),ones(nnz(M.Phi),1),nVoxels,nFibers);
 sumwf = A*w;
 sumwf(sumwf>1) = 1;
 
 D0 = 3; %[Alexander, 2001] water free isotropic diffusion
 
 % Compute isotropic weight (w0) and microstructure tensor (Qa) per voxel
-w0 = zeros(Nvoxels,1);
-wa = zeros(Nvoxels,1);
-Qa = cell(Nvoxels,1);
-parfor v=1:Nvoxels
+w0 = zeros(nVoxels,1);
+wa = zeros(nVoxels,1);
+Qa = cell(nVoxels,1);
+parfor v=1:nVoxels
     rangewf = linspace(1e-9,1-sumwf(v)-1e-9,100);
     [w0(v), Qa{v}] = fit_iso_micro(S0(v), bval, D0, bvecs', dSig(:,v), Sf(:,v), rangewf, sumwf(v));
     wa(v) = 1 - w0(v) - sumwf(v);
-    disp(['voxel ', num2str(v),'/',num2str(Nvoxels)])   
+    disp(['voxel ', num2str(v),'/',num2str(nVoxels)])   
 end
 
 fit.w0                  = w0;
